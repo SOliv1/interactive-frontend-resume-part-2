76aeb199d097a9ab5689e2ae8beedc56216a23a0
Ubuntu <ubuntu@ip-172-31-29-253.ec2.internal>
2019-09-16 17:14:43 +0000

    Fetch repo information for users

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -13,13 +13,31 @@ function userInformationHTML(user) {
             </div>
             <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
         </div>`;
-
 }
 
 
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
+}
 
 function fetchGitHubInformation(event) {
-
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -28,18 +46,23 @@ function fetchGitHubInformation(event) {
 
     }
 
-
     $("#gh-user-data").html(
         `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
-
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
 
-        
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
+            $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
+        },
+
         function(response) {
             var userData = response;
             $("#gh-user-data").html(userInformationHTML(userData));
